d6bb3ba71c4f7646b1ffaa5c074ad86a7e1ac257
2Dokk <2dokyoung@gmail.com>
2022-04-12 22:29:54 +0900

    20220412

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+//Adding Compoenents
+import Song from "./components/Song";
+import Player from "./components/Player"
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }

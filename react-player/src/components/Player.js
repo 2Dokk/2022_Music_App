f2d1e0a0e67972b5781561ac2e874e2de75bc0d0
2Dokk <2dokyoung@gmail.com>
2022-04-23 19:52:59 +0900

    20220423

--- react-player/src/components/Player.js
+++ react-player/src/components/Player.js
@@ -20,15 +20,15 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     }
   };
   const timeUpdateHandler = (e) => {
-      const current = e.target.currentTime;
-      const duration = e.target.duration;
-      setSongInfo({...songInfo, currentTime: current, duration})
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
   };
   const getTime = (time) => {
-      return(
-          Math.floor(time / 60)+ ":" + ("0" + Math.floor(time % 60)).slice(-2)
-      )
-  }
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
   //State
   const [songInfo, setSongInfo] = useState({
     currentTime: null,
@@ -38,7 +38,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input min={0} max={songInfo.duration} type="range" />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">

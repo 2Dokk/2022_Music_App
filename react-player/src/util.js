ea3a21af8bc2ac91187aee8f5a1ed4a5414cdbf7
2Dokk <2dokyoung@gmail.com>
2022-05-04 23:53:21 +0900

    20220504

---
+++ react-player/src/util.js
@@ -0,0 +1,10 @@
+const playAudio = () => {
+  if (isPlaying) {
+    const playPromise = audioRef.current.play();
+    if (playPromise !== undefined) {
+      playPromise.then((audio) => {
+        audioRef.current.play();
+      });
+    }
+  }
+};

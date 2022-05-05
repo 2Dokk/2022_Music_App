a2417dd616d79906f3daf943fc45f01539123c14
2Dokk <2dokyoung@gmail.com>
2022-05-06 00:37:05 +0900

    20220504

--- react-player/src/util.js
+++ react-player/src/util.js
@@ -1,10 +1,12 @@
-const playAudio = () => {
+export const playAudio = (isPlaying, audioRef) => {
   if (isPlaying) {
     const playPromise = audioRef.current.play();
     if (playPromise !== undefined) {
-      playPromise.then((audio) => {
-        audioRef.current.play();
-      });
+      playPromise
+        .then((audio) => {
+          audioRef.current.play();
+        })
+        .catch((error) => console.log(error));
     }
   }
 };

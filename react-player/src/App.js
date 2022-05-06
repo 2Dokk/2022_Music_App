a757920f7d98ed23395dc673e8d0a9f41c8cb5bb
2Dokk <2dokyoung@gmail.com>
2022-05-06 23:16:07 +0900

    20220506

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -19,11 +19,14 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
+    //Calculate percentage
+    const roundedCurrent = Math.round(current);
     setSongInfo({ ...songInfo, currentTime: current, duration });
   };
 

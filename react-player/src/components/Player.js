1889bf723ec399af436ad7294ff7825d3de381f9
2Dokk <2dokyoung@gmail.com>
2022-05-09 01:05:26 +0900

    20220509

--- react-player/src/components/Player.js
+++ react-player/src/components/Player.js
@@ -60,11 +60,13 @@ const Player = ({
     }
     if (direction === "skip-back") {
       if ((currentIndex - 1) % songs.length === -1) {
-        setCurrentSong(songs[songs.length - 1]);
+        await setCurrentSong(songs[songs.length - 1]);
+        activeLibraryHandler(songs[songs.length - 1]);
         if (isPlaying) audioRef.current.play();
         return;
       }
-      setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex - 1) % songs.length]);
     }
     if (isPlaying) audioRef.current.play();
   };

d81de160af40e63b71449a128273de0c0718247a
2Dokk <2dokyoung@gmail.com>
2022-05-09 00:24:11 +0900

    20220508

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -37,7 +37,7 @@ function App() {
     });
   };
   const songEndHandler = async () => {
-    let currentIndex = songs.findIndex((song) => song.id === currentSong);
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
     await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
     if (isPlaying) audioRef.current.play();
   };
